b8ccba6a3fc3055e6d7e372f36a4bfe8f6d2e20c
Joe Davison <joedavison98@gmail.com>
2023-04-28 12:18:23 +0100

    Multiple notes, across octaves

--- transposer.py
+++ transposer.py
@@ -1,20 +1,30 @@
 LETTER_NOTES = ('C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'Bb', 'B')
-INT_NOTES = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+INT_NOTES = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
 def integer_to_letter(int):
+    while int > 11:
+        int = int - 12
+    while int < 0:
+        int = int + 12
     return LETTER_NOTES[int]
 
 def letter_to_integer(letter):
     index = LETTER_NOTES.index(letter)
     return INT_NOTES[index]
 
-before_letter = input("Enter note to be transposed:")
-before_int = letter_to_integer(before_letter)
+def transpose_note(note, semitones):
+    before_int = letter_to_integer(note)
+    after_int = before_int + semitones
+    after_letter = integer_to_letter(after_int)
+    return after_letter
 
-semitones = int(input("Enter degree of transposition:"))
+notes_str = input('Enter notes to be transposed seperated by spaces: ')
+semitones = int(input('Enter transposition in semitones: '))
+notes = notes_str.split(' ')
+result = []
 
-after_int = before_int + semitones
-after_letter = integer_to_letter(after_int)
+for note in notes:
+    result.append(transpose_note(note, semitones))
 
-print(after_letter)
+print(' '.join(result))
 
